caf616b158a3a8737633d3b494f2fb2c735d2fe7
nmorduch <1051450+nmorduch@users.noreply.github.com>
2024-07-25 22:05:59 +0100

    Add embed

--- src/index.js
+++ src/index.js
@@ -1,28 +1,22 @@
 import './index.scss';
 
-let queue = [];
-let data = null;
-
 const settings = {
   'viz__id': (el) => {
-    // use data
+    let a = el.querySelector('.dataviz__title-container')
+    let b = el.querySelector('.dataviz__chart-container')
+    a && a.remove()
+    b && b.remove()
+
+    let target = document.createElement("script")
+    target.src = "https://juliacraven.shorthandstories.com/black-maternal-and-child-health/embed.js"
+    el.appendChild(target)
   }
 };
 
-fetch('endpoint').then(response => response.json()).then((_data)=>{
-  data = _data;
-  for(let i=0; i<queue.length; i++)
-    queue[i]();
-});
 
 window.renderDataViz = function(el){
   let id = el.getAttribute('id');
   let chart = settings[id];
   if(!chart) return;
-
-  if(data){
-    chart(el);
-  } else {
-    queue.push(() => chart(el));
-  }
+  chart(el);
 }
